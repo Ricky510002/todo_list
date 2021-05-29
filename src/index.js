9d1625d632e175babaa3b8705fafbadc0551d5a5
Ryuki Goto <rth510ryu@gmail.com>
2021-05-29 10:23:14 +0900

    データの永続化の機能追加

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {App} from './App';
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBBaA7zoCg21aRpnvpuAN728_Qfd9K9zWM",
+    authDomain: "todolist-ca051.firebaseapp.com",
+    projectId: "todolist-ca051",
+    storageBucket: "todolist-ca051.appspot.com",
+    messagingSenderId: "646142315728",
+    appId: "1:646142315728:web:2f34c657877b98dfbe22d5",
+    measurementId: "G-LXPH6SVKZL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 
 ReactDom.render(<App/>,document.getElementById("root"));
