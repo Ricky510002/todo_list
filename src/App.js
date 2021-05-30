47d8609d863f4f662991c21c6cb778ae4a1ff640
Ryuki Goto <rth510ryu@gmail.com>
2021-05-30 20:53:44 +0900

    認証機能の修正

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import "./App.css";
-import { Todo } from "./Todo.js";
+import { Todo } from "./Todo";
 import "firebase/firestore";
 import { auth, provider } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export const App = () => {
-  const [user, loading, error] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
   const signIn = () => {
     auth.signInWithPopup(provider).catch((err) => alert(err.message));
   };
   return (
     <div className="app">
-      {user ? (<Todo/>):(<button onClick={signIn} className="sign_in">Sign in</button>)
-      }
+      {/* {user ? (
+        <CreateTodo />
+      ) : (
+        <button onClick={signIn} className="sign_in">
+          Sign up
+        </button>
+      )} */}
+
+      {user ? (
+        <Todo />
+      ) : (
+        <button onClick={signIn} className="sign_in">
+          Login
+        </button>
+      )}
     </div>
   );
 };
